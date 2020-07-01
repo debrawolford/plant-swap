918d61418f54bc4b67f947274541691312ec1a57
Debra Wolford <debra.wolford@gmail.com>
2020-07-01 18:09:32 -0500

    Installed and tested basic Flask app

--- app.py
+++ app.py
@@ -1,3 +1,21 @@
-name = input("your name?")
-print("Hello", name)
+import os
+from flask import Flask
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
+if os.path.exists("env.py"):
+    import env
+
+app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = "plant_swap"
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+
+
+@app.route("/")
+def home():
+    return "Home"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
