f669af2b9b36e8032d41ed0c329b5fdd5307eadd
Debra Wolford <debra.wolford@gmail.com>
2020-07-03 16:43:58 -0500

    Added basic Bootstrap styling to posts.html

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, redirect, request, url_for, jsonify
+from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from passlib.hash import pbkdf2_sha256
@@ -13,10 +13,13 @@ app = Flask(__name__)
 app.config["MONGO_DBNAME"] = "plant_swap"
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 
+mongo = PyMongo(app)
+
 
 @app.route("/")
-def hello():
-    return "Hello World"
+@app.route("/get_posts")
+def get_posts():
+    return render_template("posts.html", posts=mongo.db.posts.find())
 
 
 if __name__ == "__main__":
