f0da5bb34d051799fb83007cd771a5be6b524a8d
Debra Wolford <debra.wolford@gmail.com>
2020-07-04 15:45:13 -0500

    Added functionality to add posts

--- app.py
+++ app.py
@@ -22,6 +22,24 @@ def get_posts():
     return render_template("posts.html", posts=mongo.db.posts.find())
 
 
+@app.route("/add_post")
+def add_post():
+    return render_template("addpost.html")
+
+
+@app.route("/submit_post", methods=["POST"])
+def submit_post():
+    posts = mongo.db.posts
+    posts.insert_one(request.form.to_dict())
+    return redirect(url_for("get_posts"))
+
+
+@app.route("/edit_post/<post_id>")
+def edit_post(post_id):
+    the_post = mongo.db.posts.find_one({"_id": ObjectId(post_id)})
+    return render_template("editpost.html", post=the_post)
+
+
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP"), port=os.environ.get("PORT"), debug=True,
